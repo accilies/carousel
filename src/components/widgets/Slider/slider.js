fe063111fc80ca9bd91adbbdb42451a943b8153c
Shoeb Surya <ssurya@gcaa.gov.ae>
2022-10-24 16:38:09 +0400

    Added API call to fetch from flicker

--- src/components/widgets/Slider/slider.js
+++ src/components/widgets/Slider/slider.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Arrows from "./components/controls/arrow";
 import SlidesList from "./components/slideList";
+import { getImages } from "../../../imagesApi";
 
 import "./styles.scss";
 
 const Slider = () => {
   const width = 500;
   const height = 500;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const images = await getImages();
+      setItems(images);
+    };
+    loadData();
+  }, []);
 
   return (
     <div style={{ width, height }} className="slider">
---
+++ src/imagesApi.js
@@ -0,0 +1,49 @@
+/**
+ * Get cats pictures
+ * @param {number} length
+ * @returns {Promise<Array<{ title: string, url:string }>}
+ */
+
+const yourApiKey = "cd596b5fd7f3f282f291a6e08c0dcc42";
+
+const data = {
+  method: "flickr.photos.search",
+  api_key: yourApiKey,
+  text: "cat", // Search Text
+  sort: "interestingness-desc",
+  per_page: 12,
+  license: "4",
+  extras: "owner_name,license",
+  format: "json",
+  nojsoncallback: 1,
+};
+
+const parameters = new URLSearchParams(data);
+
+const url = `https://api.flickr.com/services/rest/?${parameters}`;
+
+const getFlickrImageURL = (photo, size) => {
+  let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
+  if (size) {
+    // Configure image size
+    url += `_${size}`;
+  }
+  url += ".jpg";
+  return url;
+};
+
+export const getImages = (length = 10) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const images = [];
+      //   const title = c?.description;
+      //   const url = c?.image?.url;
+
+      //   images.push({ title, url });
+      // get an array of image-url
+      data.photos.photo.map((photo) => {
+        return images.push(getFlickrImageURL(photo, "q"));
+      });
+    });
+};
