e5b0bcc4a40934ed2a0deb292700f61e72330eb9
Shoeb Surya <ssurya@gcaa.gov.ae>
2022-10-24 15:58:21 +0400

    Added sass package + added slides SCSS with arrows and core layout for the slides

--- src/components/widgets/Slider/slider.js
+++ src/components/widgets/Slider/slider.js
@@ -1,12 +1,28 @@
 import React from "react";
 
+import "./styles.scss";
+
 const Slider = () => {
-  const width = 100;
-  const height = 100;
+  const width = 500;
+  const height = 500;
 
   return (
     <div style={{ width, height }} className="slider">
+      <div className="arrows">
+        <div className="arrow left" />
+        <div className="arrow right" />
+      </div>
+
       <div className="slide-list">
+        <div className="slide">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg"
+            alt="Alt Text"
+            className="slide-image"
+          />
+          <div className="slide-title">Title</div>
+        </div>
+
         <div className="slide">
           <img
             src="https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg"
