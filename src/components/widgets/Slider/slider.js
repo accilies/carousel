198c61eebfc29253134aeab45da1222645fffb36
Shoeb Surya <ssurya@gcaa.gov.ae>
2022-10-24 15:44:58 +0400

    Basic Slider core structure

--- src/components/widgets/Slider/slider.js
+++ src/components/widgets/Slider/slider.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 const Slider = () => {
+  const width = 100;
+  const height = 100;
+
   return (
-    <>
-      <div> Dev Start</div>
-    </>
+    <div style={{ width, height }} className="slider">
+      <div className="slide-list">
+        <div className="slide">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg"
+            alt="Alt Text"
+            className="slide-image"
+          />
+          <div className="slide-title">Title</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
